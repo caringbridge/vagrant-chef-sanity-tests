09e79b6e90567e9c7b19a628ea813dade7a090f3
CBwyatt <wandersen@caringbridge.org>
2014-07-31 13:53:51 -0500

    Site and profile

--- sanity_tests.rb
+++ sanity_tests.rb
@@ -1,12 +1,26 @@
+# Does http://www.caringbridge.dev/ resolve?
+# Can you create an account?
+# Did you receive the welcome emails after creating your account?
+# Can you sign up for journal notifications?
+# Can you create a site and add a journal with a photo?
+# Did you receive a journal notification?
+# Can you create a PDF of your site?
+# Can you delete the photo from your journal?
+
 require 'watir-webdriver'
 
 @b = Watir::Browser.new :ff
 
-# Does caringbridge.dev resolve?
+print "Does http://www.caringbridge.dev/ resolve? \n"
 @b.goto 'http://www.caringbridge.dev'
-# @todo: check if page exists
 
-# Can you create a profile?
+if @b.div(:id => 'dev-performance').when_present.text.include? "[DEV:Host centos6.caringbridge.dev]"
+		print "Test passed!\n"
+else
+		print "Test failed!\n"
+end
+
+print "Can you create an account? \n"
 @b.goto 'https://www.caringbridge.dev/signup'
 
 @b.text_field(:name => 'firstName').set 'Vagrant'
@@ -17,7 +31,24 @@ require 'watir-webdriver'
 @b.button(:name => 'submit-btn').click
 
 if @b.span(:class => 'user-generated global-profile-firstname').when_present.text == 'Vagrant'
-		print 'Test passed!'
+		print "Test passed!\n"
 else
-		print 'Test failed!'
+		print "Test failed!\n"
+end
+
+print "Can you start a site? \n"
+@b.goto 'https://www.caringbridge.dev/createwebsite'
+
+@b.checkbox(:name => 'terms', :value => '1').set
+@b.text_field(:name => 'firstName').set 'Vagrant'
+@b.text_field(:name => 'lastName').set 'Chef'
+@b.radio(:id => 'privacy-low', :value => 'low').set
+@b.radio(:id => 'isSearchable', :value => '1').set
+@b.button(:name => 'submit-btn').click
+
+# @todo: Defaults to v3, when it defaults to v5 we can update this...
+if @b.span(:class => 'user-generated global-profile-firstname').when_present.text == 'Vagrant'
+		print "Test passed!\n"
+else
+		print "Test failed!\n"
 end
