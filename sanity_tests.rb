27e085143557e19412a9296711cc2020475c0942
CBwyatt <wandersen@caringbridge.org>
2014-08-13 15:28:29 -0500

    Search testing

--- sanity_tests.rb
+++ sanity_tests.rb
@@ -1,15 +1,20 @@
-# Does http://www.caringbridge.dev/ resolve?
-# Can you create an account?
+# X Does http://www.caringbridge.dev/ resolve?
+# X Can you create an account?
 # Did you receive the welcome emails after creating your account?
+# X Can you create a site 
+# X Can you search for your site?
+# Can you add a journal with a photo?
 # Can you sign up for journal notifications?
-# Can you create a site and add a journal with a photo?
 # Did you receive a journal notification?
 # Can you create a PDF of your site?
 # Can you delete the photo from your journal?
 
+
 require 'watir-webdriver'
 
 @b = Watir::Browser.new :ff
+
+@b.goto 'http://www.caringbridge.dev/search?q=Vagrant' 
 
 print "Does http://www.caringbridge.dev/ resolve? \n"
 @b.goto 'http://www.caringbridge.dev'
@@ -42,6 +47,7 @@ print "Can you start a site? \n"
 @b.checkbox(:name => 'terms', :value => '1').set
 @b.text_field(:name => 'firstName').set 'Vagrant'
 @b.text_field(:name => 'lastName').set 'Chef'
+@b.text_field(:name => 'name').set 'Chef' + Time.now.to_i.to_s
 @b.radio(:id => 'privacy-low', :value => 'low').set
 @b.radio(:id => 'isSearchable', :value => '1').set
 @b.button(:name => 'submit-btn').click
@@ -52,3 +58,20 @@ if @b.span(:class => 'user-generated global-profile-firstname').when_present.tex
 else
 		print "Test failed!\n"
 end
+
+# Fix inexplicable browser hang
+# @todo: Fix inexplicable browser hang 
+@b.close
+@b = Watir::Browser.new :ff
+
+print "Can you search for your site? \n"
+@b.goto 'http://www.caringbridge.dev/search?q=Vagrant' 
+
+if @b.text.include? 'Is one of these the Site or SupportPlanner you are looking for?'
+		print "Test passed!\n"
+else
+		print "Test failed!\n"
+end
+
+# Wrap up
+@b.close
