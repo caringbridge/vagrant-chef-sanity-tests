b8fff0d0782f47a16901115b607bf0c0b29a9442
Joe Gliniecki <jgliniecki@caringbridge.org>
2014-10-31 13:41:58 -0500

    Watir test Add Photo to Journal

--- sanity_tests.rb
+++ sanity_tests.rb
@@ -13,9 +13,11 @@
 require 'watir-webdriver'
 
 @b = Watir::Browser.new :ff
+@caringBridgeUrl = 'http://www.caringbridge.dev';
+#@caringBridgeUrl = 'http://staging.caringbridge.org';
 
-print "Does http://www.caringbridge.dev/ resolve? \n"
-@b.goto 'http://www.caringbridge.dev'
+print "Does #{@caringBridgeUrl} resolve? \n"
+@b.goto "#{@caringBridgeUrl}"
 
 if @b.div(:id => 'dev-performance').when_present.text.include? "[DEV:Host centos6.caringbridge.dev]"
 		print "Test passed!\n"
@@ -24,10 +26,10 @@ else
 end
 
 print "Can you create an account? \n"
-@b.goto 'https://www.caringbridge.dev/signup'
+@b.goto "#{@caringBridgeUrl}/signup"
 
 testUserEmail = 'cbvagrantchef' + Time.now.to_i.to_s
-
+puts "testUserEmail: #{testUserEmail}";
 @b.text_field(:name => 'firstName').set 'Vagrant'
 @b.text_field(:name => 'lastName').set 'Chef'
 @b.text_field(:name => 'email').set testUserEmail + '@mailinator.com'
@@ -52,7 +54,7 @@ else
 end
 
 print "Can you start a site? \n"
-@b.goto 'https://www.caringbridge.dev/createwebsite'
+@b.goto "#{@caringBridgeUrl}/createwebsite"
 
 @b.checkbox(:name => 'terms', :value => '1').set
 @b.text_field(:name => 'firstName').set 'Vagrant'
@@ -75,7 +77,7 @@ end
 @b = Watir::Browser.new :ff
 
 print "Can you search for your site? \n"
-@b.goto 'http://www.caringbridge.dev/search?q=Vagrant' 
+@b.goto "#{@caringBridgeUrl}/search?q=Vagrant"
 
 if @b.text.include? 'Is one of these the Site or SupportPlanner you are looking for?'
 		print "Test passed!\n"
@@ -85,5 +87,50 @@ else
 		"Still not working? vagrant destroy && vagrant up \n"
 end
 
-# Wrap up
+@b.close
+@b = Watir::Browser.new :ff
+
+print "\n *** Can you log in to your account? *** \n"
+@b.goto "#{@caringBridgeUrl}/signin"
+@b.text_field(:name => 'email').set testUserEmail + '@mailinator.com'
+@b.text_field(:name => 'password').set '123456'
+@b.checkbox(:name => 'remember-me').clear
+@b.button(:name => 'submit-btn').click
+@b.wait_until { @b.body.exists? }
+  
+if @b.text.include? 'Vagrant Chef'
+    print "Test passed! Able to log in to account\n"
+else
+    print "Test failed! Unable to log in to account\n"
+end
+
+print "\n *** Can you create your first journal entry from the welcome page? *** \n"
+@b.div(:class_name => 'site').link(:text => 'Vagrant Chef').click # Navigate to your caring bridge site
+@b.wait_until { @b.body.exists? }
+@b.text_field(:id => 'title').set 'My First Journal Title'
+@b.iframe(:class_name => 'wysihtml5-sandbox').body.send_keys 'My first journal text'
+@b.button(:name => 'submit-btn').click
+@b.wait_until { @b.body.exists? }
+
+if @b.text.include? 'My First Journal Title'
+    print "Test passed creating first journal entry from welcome page\n"
+else
+    print "Test failed creating first journal entry from welcome page\n"
+end
+
+print "\n *** Can you add a journal entry with a photo? *** \n"
+@b.link(:href => /journal/).click # Navigate to journal tab
+@b.wait_until { @b.body.exists? }
+@b.link(:id => 'bar-action-photo').click
+@b.file_field(:id => 'photo-upload').set File.expand_path(File.dirname(__FILE__) + '/pig.jpg')
+@b.text_field(:id => 'title').set 'Test journal with photo'
+@b.iframe(:class_name => 'wysihtml5-sandbox').body.send_keys 'My journal text'
+@b.button(:name => 'submit-btn').click
+
+if @b.figure.div.present?
+    print "Test passed creating journal entry with a photo\n"
+else
+    print "Test failed creating journal entry with a photo\n"
+end
+
 @b.close
